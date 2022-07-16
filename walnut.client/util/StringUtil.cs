701a674bafb3b375e6e02dfee9e2cd6a226ba566
nyakori <935419854@qq.com>
2022-07-16 13:05:13 +0800

    add(all): 添加字符串扩展

--- walnut.client/util/StringUtil.cs
+++ walnut.client/util/StringUtil.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace walnut.client.util
 {
     public static class StringUtil
     {
+        /// <summary>
+        /// 时间格式化转换器
+        /// </summary>
+        static IsoDateTimeConverter dateTimeConverter = new IsoDateTimeConverter { DateTimeFormat = "yyyy-MM-dd HH:mm:ss" };
+
         /// <summary>
         /// 获取字节码
         /// </summary>
@@ -105,6 +114,98 @@ namespace walnut.client.util
             {
                 return defaultValue;
             }
+        }
+
+        /// <summary>
+        /// 转为数值
+        /// </summary>
+        /// <param name="source">原始字符串</param>
+        /// <returns>转换目标</returns>
+        public static T toJson<T>(this String source)
+        {
+            return JsonConvert.DeserializeObject<T>(source, dateTimeConverter);
+        }
+
+        /// <summary>
+        /// 将json字符串转为对象
+        /// </summary>
+        /// <param name="s"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T toJson<T>(this string source, T defaultValue)
+        {
+            try
+            {
+                return source.toJson<T>();
+            }
+            catch (Exception)
+            {
+                return default(T);
+            }
+        }
+
+        /// <summary>
+        /// 转为数值
+        /// </summary>
+        /// <param name="source">原始字符串</param>
+        /// <returns>转换目标</returns>
+        public static T toXml<T>(this String source)
+        {
+            // 加载xml字符串
+            var xmlDocument = new XmlDocument();
+            xmlDocument.LoadXml(source);
+            String jsonString = JsonConvert.SerializeXmlNode(xmlDocument);
+
+            // 反序列化
+            return JsonConvert.DeserializeObject<T>(jsonString, dateTimeConverter);
+        }
+
+        /// <summary>
+        /// 将json字符串转为对象
+        /// </summary>
+        /// <param name="s"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T toXml<T>(this string source, T defaultValue)
+        {
+            try
+            {
+                return source.toXml<T>();
+            }
+            catch (Exception)
+            {
+                return default(T);
+            }
+        }
+
+        /// <summary>
+        /// base64加密
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static String base64Encode(this string source)
+        {
+            if (string.IsNullOrEmpty(source))
+            {
+                return string.Empty;
+            }
+
+            return Convert.ToBase64String(source.getBytes());
+        }
+
+        /// <summary>
+        /// base64解密
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static String base64Decode(this string source)
+        {
+            if (string.IsNullOrEmpty(source))
+            {
+                return string.Empty;
+            }
+
+            return Encoding.UTF8.GetString(Convert.FromBase64String(source));
         }
     }
 }
