dcae1d1d7cf9a32e89ddd92accaf29999db93142
nyakori <935419854@qq.com>
2023-03-27 09:42:31 +0800

    fix(ui): 派生treeView

--- walnut.client/ui/WalnutTreeView.cs
+++ walnut.client/ui/WalnutTreeView.cs
@@ -1,42 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Drawing;
 using System.Data;
-using System.Linq;
+using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 
 namespace walnut.client.ui
 {
-    public partial class WalnutTreeView : UserControl
+    public partial class WalnutTreeView : TreeView
     {
         public WalnutTreeView()
         {
             InitializeComponent();
-        }
-
-        private void treeView_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
-        {
-            if (this.NodeMouseDoubleClick != null) this.NodeMouseDoubleClick(sender, e);
-        }
-
-        public event TreeNodeMouseClickEventHandler NodeMouseDoubleClick;
-
-        /// <summary>
-        /// 创建族长节点
-        /// </summary>
-        /// <param name="name">节点标识</param>
-        /// <returns>族长</returns>
-        public WalnutTreeNode createPatriarchy()
-        {
-            // 构造节点
-            var patriarchy = new WalnutTreeNode();
-
-            // 建立关联
-            this.treeView.Nodes.Add(patriarchy);
-
-            return patriarchy;
         }
     }
 }
