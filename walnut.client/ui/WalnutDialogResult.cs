699ef1875a04689477ed1cec075f800001029d0a
nyakori <935419854@qq.com>
2023-04-18 16:19:23 +0800

    add(ui): 添加用于传递会话结果的对象

---
+++ walnut.client/ui/WalnutDialogResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace walnut.client.ui
+{
+    public class WalnutDialogResult
+    {
+        /// <summary>
+        /// 用于展示的值
+        /// </summary>
+        public String mask { get; set; } = String.Empty;
+
+        /// <summary>
+        /// 实际选中的对象
+        /// </summary>
+        public Object result { get; set; } = null;
+    }
+}
